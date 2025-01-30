275dbd5b8432b5c77a7b07b7e047194e06d5ffd5
Chiara Monforte <monforte.chiara.mc@gmail.com>
2025-01-30 16:20:15 +0100

    [FEAT] Create mission summary sheet (#173)
    * [FEAT] Create mission summary sheet
    
    * Move files and address inputs from comments
    
    * Adjust summary figure in case of missing data and attributes
    
    * Adjust creation of the second table
    
    * Adjust profile limits in basic vars plot
    
    * Adjust default dir
    
    * Add tests
    
    * Adjusted platform serial
    
    * Adjust test.yml
    
    * Debug test.yml
    
    * Debug test.yml
    
    * Debug test.yml
    
    * Debug test.yml
    
    * Debug test.yml
    
    * Debug test.yml
    
    * Try test.yml
    
    * Try test.yml
    
    * Force not to create plots
    
    * Force not to create plots
    
    * Add OS and Windows to test
    
    ---------
    
    Co-authored-by: ChiaraMonforte <chiara.monforte@ad.cen.uni-hamburg.de>

---
+++ tests/test_mission_summary.py
@@ -0,0 +1,22 @@
+from glidertest import fetchers, summary_sheet
+import pandas as pd
+import matplotlib
+
+matplotlib.use('agg')  # use agg backend to prevent creating plot windows during tests
+
+def test_qc_checks():
+    ds = fetchers.load_sample_dataset()
+    gr, spike, flat, err = summary_sheet.qc_checks(ds, var='PSAL')
+def test_tableqc():
+    ds = fetchers.load_sample_dataset()
+    df_test = pd.DataFrame({'': ['Global range', 'Spike test', 'Flat test', 'Hysteresis', 'Drift'],
+                        'Temperature': ['✓', '✓', '✓', '✓', '✓'],
+                        'Salinity': ['✓', '✓', '✓', '✓', '✓']})
+    tableT = summary_sheet.fill_tableqc(df_test,ds, var='TEMP')
+def test_phrase_duration_check():
+    ds = fetchers.load_sample_dataset()
+    summary_sheet.phrase_numberprof_check(ds)
+    summary_sheet.phrase_duration_check(ds)
+def test_summary_plot():
+    ds = fetchers.load_sample_dataset()
+    summary_sheet.summary_plot(ds, test=False)
