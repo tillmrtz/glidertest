ded95473a441e7859664b6ca039dc339b71e3448
Callum Rollo <c.rollo@outlook.com>
2025-02-27 09:59:48 +0100

    many more seaexplorer datasets (#209)
    * many more seaexplorer datasets
    
    * order datasets alphabetically and correct hashes

--- glidertest/fetchers.py
+++ glidertest/fetchers.py
@@ -1,17 +1,24 @@
 import pooch
 import xarray as xr
 
-server = "https://callumrollo.com/files"
+server = "https://erddap.observations.voiceoftheocean.org/examples/glidertest"
 data_source_og = pooch.create(
     path=pooch.os_cache("glidertest"),
     base_url=server,
     registry={
-        "sea055_20220104T1536_delayed.nc": "sha256:7f72f8a0398c3d339687d7b7dcf0311036997f6855ed80cae5bbf877e09975a6",
-        "sea045_20230530T0832_delayed.nc": "sha256:9a97b5b212e9ad751909e175bc01fdc75fd7b7fd784cc59d8e9f29e8bdfb829f",
+        "sea045_20230530T0832_delayed.nc": "sha256:6d3075280d2e4a075d011b59486d7fa6bf616459dc92a7ece14a9d9a749f5ce4",
         "sea045_20230604T1253_delayed.nc": "sha256:9d71b5fb580842e8296b447041cd5b544c8b4336df40a8f3c2e09279f436f502",
-        "sg015_20050213T230253_delayed.nc": "sha256:ca64e33e9f317e1fc3442e74485a9bf5bb1b4a81b5728e9978847b436e0586ab",
-        "sg014_20040924T182454_delayed.nc": "sha256:c9fca08ce676573224c04512f4d5bfe251d0419478ee433dfefa03aa70e2eb9a",
-        "sg014_20040924T182454_delayed_subset.nc": "sha256:0e97a4107364d27364d076ed8007f08c891b2015b439cf524a44612de0a1a2ea",
+        "sea055_20220104T1536_delayed.nc": "sha256:7f72f8a0398c3d339687d7b7dcf0311036997f6855ed80cae5bbf877e09975a6",
+        "sea055_20240628T0856_delayed.nc": "sha256:7756a0034dba7241dd626b7e21cfb1d0bce634865a5f3dceea391faaa3195546",
+        "sea055_20241009T1345_delayed.nc": "sha256:cd411c326b2734efd73a02b97e43c019dccbe1825447542b1d7cbfa4130639e4",
+        'sea063_20230811T1657_delayed.nc': 'sha256:8d4bf2555f226a35078290961c49749389fb0f9075917b263b3231823f87fc35',
+        'sea067_20221113T0853_delayed.nc': 'sha256:641856ab415b06743d43609fd4f77c07e382087741cf068afd0bbf3c1f425402',
+        'sea069_20230726T0628_delayed.nc': 'sha256:a502372d0f16e5cdba13c76167dc3adceeb45342f5f53edbbdf131cbd728824e',
+        'sea077_20220906T0748_delayed.nc': 'sha256:3c2e9def777a2e230cc1b2114ca069dcbdb3c58ada2794a8305a781f3f76da01',
+        'sea077_20230316T1019_delayed.nc': 'sha256:a146247dd33d38ab7b1d379481f66c749fdf16db31aacad1fe70287c55b359a6',
+        'sg014_20040924T182454_delayed.nc': 'sha256:c9fca08ce676573224c04512f4d5bfe251d0419478ee433dfefa03aa70e2eb9a',
+        'sg014_20040924T182454_delayed_subset.nc': 'sha256:0e97a4107364d27364d076ed8007f08c891b2015b439cf524a44612de0a1a2ea',
+        'sg015_20050213T230253_delayed.nc': 'sha256:ca64e33e9f317e1fc3442e74485a9bf5bb1b4a81b5728e9978847b436e0586ab',
     },
 )
 
