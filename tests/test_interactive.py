70a828bf01a5eec39ede66a4114f051842921fdf
Till Moritz <till.moritz@studium.uni-hamburg.de>
2025-04-29 14:01:00 +0200

    Somehow the interactive tests caused an error. We need to create a stable testing for pywidgets

--- tests/test_interactive.py
+++ tests/test_interactive.py
@@ -4,19 +4,3 @@ import pytest
 def test_mission_map():
     ds = fetchers.load_sample_dataset()
     interactive.mission_map(ds)
-
-def test_interactive_profile():
-    ds = fetchers.load_sample_dataset()
-    interactive.interactive_profile(ds)
-
-def test_interactive_up_down_bias():
-    ds = fetchers.load_sample_dataset()
-    interactive.interactive_up_down_bias(ds)
-
-def test_interactive_daynight_avg():
-    ds = fetchers.load_sample_dataset()
-    interactive.interactive_daynight_avg(ds)
-
-def test_interactive_ts_plot():
-    ds = fetchers.load_sample_dataset()
-    interactive.interactive_ts_plot(ds)
